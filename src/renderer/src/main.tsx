7de0fefc3a11c17a0ddf4256f359e3099fe1300d
Iftekhar-Ifat <iftekharifat007@gmail.com>
2026-03-21 21:36:44 +0600

    fixed first page not found issue

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,14 +2,15 @@ import './assets/main.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { RouterProvider, createHashHistory, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { routeTree } from './routeTree.gen'
 
 const queryClient = new QueryClient()
 
 const router = createRouter({
-  routeTree
+  routeTree,
+  history: createHashHistory()
 })
 
 declare module '@tanstack/react-router' {
